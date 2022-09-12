1bdc273b13567a1895ef3b5defee71f6618c5dc3
mohammad hossain zarei <hoeinmhz986@gmail.com>
2022-09-13 00:01:37 +0430

    edit lib

--- Accountants.py
+++ Accountants.py
@@ -3,7 +3,7 @@ import desk_lib
 import user_lib
 import product_lib
 
-# desk_lib.desk_write(31)
+desk_lib.desk_write(31)
 product_lib.product_write("petza", 65000 , 130000)
-# user_lib.user_write("hosein","zarie","hoseinmhz986@gmail.com","213987d")
-# invoice_lib.invoice_wriet( "pasta" , 1 ,19)
+user_lib.user_write("hosein","zarie","hoseinmhz986@gmail.com",21398734)
+invoice_lib.invoice_wriet( "pasta" , 1 ,19)
--- user_lib.py
+++ user_lib.py
@@ -2,7 +2,7 @@ import db_lib
 import log_lib
 
 
-user_db_address = "db/user_admin"
+user_db_address = "db/user_admin.txt"
 
 def user_write(lname , fname , email , password):
     msg = []
@@ -11,8 +11,8 @@ def user_write(lname , fname , email , password):
     msg.append(fname)
     msg.append(email)
     msg.append(str(password))
-    msg.append(db_lib.Date_now)
-    deta =",".join(msg)
+    msg.append(db_lib.Date_now())
+    deta = ",".join(msg)
     deta = deta +"\n"
     
     log_lib.Log("The user is complete")
